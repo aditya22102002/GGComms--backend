712e5d5defbb44b7f52694339b0f883852be576d
aditya22102002 <khemka.adityaraj@gmail.com>
2025-04-17 19:07:24 +0530

    cors link changed

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 
 
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  // origin: "http://localhost:5173", 
+  origin:"https://ggcomms.netlify.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
