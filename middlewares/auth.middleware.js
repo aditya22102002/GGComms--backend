2eb361abd464706d95dba3e8a0743c56a16763c0
aditya22102002 <khemka.adityaraj@gmail.com>
2025-04-18 09:29:42 +0530

    logs added to auth

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -7,12 +7,16 @@ import jwt from "jsonwebtoken"
 export const verifyJwt =asyncHandler(async (req,res,next)=>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "").trim();
+        console.log("token: ",token);
+        
 
         if(!token){
             throw new ApiError(401,"Unauthorized request")
         }
     
         const decodedToken= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        console.log("decoded token: ",decodedToken);
+
     
         const user= await User.findById(decodedToken?._id).select("-password -refreshToken")
         if(!user){
